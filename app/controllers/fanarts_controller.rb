59b3ade1f42697bd9a45e59747b0d2fa7ea0f137
Brian Donovan <brian.donovan@gmail.com>
2010-03-30 21:51:21 -0700

    Fix copy-paste error.

--- app/controllers/fanarts_controller.rb
+++ app/controllers/fanarts_controller.rb
@@ -2,7 +2,7 @@ class FanartsController < ApplicationController
   def show
     movie = Movie.find(params[:movie_id])
 
-    if movie.poster.exist?
+    if movie.fanart.exist?
       expires_in 10.years, :public => true
       # send_file movie.fanart[params[:id]],
       #   :filename => movie.file.basename('.*')+'.jpg',
